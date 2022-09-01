c312102d4a09de2be79b4b14ffe7d2cb797f6033
mayur <mayurpatil2269@gmail.com>
2022-09-01 08:38:18 +0530

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,12 @@
+package pratice.two.pra;
+
+public class multiply {
+	int a=10;
+	int b=10;
+	
+	public static void main(String[] args) {
+		int a=10;
+		int b=10;
+		System.out.println("multiply="+(a*b));
+	}
+}
